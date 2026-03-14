9b7663c9db749224dc7949f3b08b8bb7dd373c08
Jamie Falconer <mrfluffyoven@gmail.com>
2026-03-14 12:56:11 +0000

    remove previously defined blob

--- m55xq-vendor.mk
+++ m55xq-vendor.mk
@@ -159,7 +159,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/m55xq/proprietary/vendor/bin/hdcp1prov:$(TARGET_COPY_OUT_VENDOR)/bin/hdcp1prov \
     vendor/samsung/m55xq/proprietary/vendor/bin/hdcp2p2prov:$(TARGET_COPY_OUT_VENDOR)/bin/hdcp2p2prov \
     vendor/samsung/m55xq/proprietary/vendor/bin/hermesd:$(TARGET_COPY_OUT_VENDOR)/bin/hermesd \
-    vendor/samsung/m55xq/proprietary/vendor/bin/hostapd_cli:$(TARGET_COPY_OUT_VENDOR)/bin/hostapd_cli \
     vendor/samsung/m55xq/proprietary/vendor/bin/hqread:$(TARGET_COPY_OUT_VENDOR)/bin/hqread \
     vendor/samsung/m55xq/proprietary/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.bluetooth@1.0-service-qti \
     vendor/samsung/m55xq/proprietary/vendor/bin/hw/android.hardware.drm@1.4-service.widevine:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.drm@1.4-service.widevine \
